06c77d9e0bf7533c8efc68753609f3e7561f7594
balavenkateshs <35548750+balavenkateshs@users.noreply.github.com>
2019-02-20 12:25:51 +0530

    Update tomcatinstall.sh

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -2,15 +2,16 @@
 sudo apt-get update
 echo "Instaling default jdk"
 sudo apt-get install default-jdk -y
-sudo apt-get install git -y
-
-echo "Create Tomcat User"
-sudo groupadd tomcat
-sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
-echo "Install Tomcat"
 cd /tmp
-sudo weget http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz
-#sudo curl -O http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.5/bin/apache-tomcat-8.5.5.tar.gz
+git clone https://github.com/balavenkateshs/DevOps.git
+cd DevOps/
+echo "Copy tomcat service into system folder"
+sudo touch /etc/systemd/system/tomcat.service
+sudo cp tomcat.service /etc/systemd/system/tomcat.service
+echo "Create Tomcat User"
+sudo useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
+echo "Install Tomcat"
+sudo wget http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz
 sudo mkdir /opt/tomcat
 sudo tar xzvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
 echo "Update Permissions"
@@ -21,12 +22,15 @@ sudo chmod g+x conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
 echo "Create a systemd Service File"
 # sudo update-java-alternatives -l # to get java path
-sudo touch /etc/systemd/system/tomcat.service
 sudo systemctl daemon-reload
+echo "Enable executable mode"
+sudo sh -c 'chmod +x /opt/tomcat//bin/*.sh'
 sudo systemctl start tomcat
 sudo systemctl status tomcat
 echo "Adjust the Firewall and Test the Tomcat Server"
 sudo ufw allow 8080
+sudo systemctl start tomcat
+echo "enable boot time "
 sudo systemctl enable tomcat
 #deb http://security.ubuntu.com/ubuntu trusty-security main universe -y
 #sudo apt-get install tomcat7
@@ -34,6 +38,6 @@ sudo systemctl enable tomcat
 
 #sudo weget http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz
 #sudo ln -s /opt/tomcat/apache-tomcat-9.0.16 /opt/tomcat/latest
-sudo sh -c 'chmod +x /opt/tomcat//bin/*.sh'
+
 
 #https://linuxize.com/post/how-to-install-tomcat-9-on-ubuntu-18-04/
